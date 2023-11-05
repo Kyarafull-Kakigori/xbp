392d663d4486511ab51559771c2455a45f43c064
Kyaraful-Kakigori <r202302128sc@jindai.jp>
2023-11-05 16:55:56 +0900

    2023.11.5

---
+++ de12/python/test.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+data = [9843672426472, 837492374265826348, 8847327436824, 183746825913784, 239856135476134]
+mean = np.mean(data)
+print("平均値:", mean)
+
+import numpy as np
+
+data = [9843672426472, 837492374265826348, 8847327436824, 183746825913784, 239856135476134]
+median = np.median(data)
+print("中央値:", median)
+
+import numpy as np
+
+data = [9843672426472, 837492374265826348, 8847327436824, 183746825913784, 239856135476134]
+std_deviation = np.std(data)
+print("標準偏差:", std_deviation)
+
+data = [9843672426472, 837492374265826348, 8847327436824, 183746825913784, 239856135476134]
+max_value = max(data)
+min_value = min(data)
+print("最大値:", max_value)
+print("最小値:", min_value)
+
